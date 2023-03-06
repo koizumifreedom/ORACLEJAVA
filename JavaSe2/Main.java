9d27d6be854cb1fd3584e2245276f7b353e9a763
小泉　友 <koizumi@freedomland.jp>
2023-03-06 16:33:48 +0900

    JavaSe2

---
+++ JavaSe2/Main.java
@@ -0,0 +1,20 @@
+//2章問1
+/*package JavaSe2;
+
+public class Main {
+    public static void main(String[] args) {
+       int val = 7;
+       bool flg = true;
+       if (flg == true) {
+           do {
+              System.out.println(val);
+           } while (val > 10);
+       }
+    }
+}*/
+ 
+//7行目で宣言しているデータ型が「bool]となってなっている為コンパイルエラーが発生します。
+//真偽値を扱うためのデータ型は「「boolean」です。
+
+//コンパイルエラー
+
